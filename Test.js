f85941c7bffef71f408eead38a71f731d7f31b80
GabrielPCF <gabrielfulber@gmail.com>
2022-09-20 22:01:06 -0400

    Week 3 Classwork

--- Test.js
+++ Test.js
@@ -1,6 +1,9 @@
-//blabla
+//Constant declarations
 const exp = require('express');
 const https = require('https');
+const path = require("path");
+const body_parser = require("body-parser");
+
 const app = exp();
 
 function test(req, res, next){
@@ -9,31 +12,39 @@ function test(req, res, next){
 }
 
 app.use(test);
-
+app.use(body_parser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    res.send("Home Page");
-
-    res.end;
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, "Home.html"));
 });
 
-// app.get("/login", function(req, res){
-//     res.send("GET: Login Page");
-// });
-
-// app.post("/login", function(req, res){
-//     res.send("POST: Login Page");
-// });
-
-app.route("/login")
-.get(function (req,res){
-    res.send("Login Page (GET)");
-    res.end;
-})
-.post(function (req,res){
-    res.send("Login Page (POST)")
-    res.end;
+app.get("/login", function(req, res){
+    res.sendFile(path.join(__dirname, "login.html"));
 });
+
+app.get("/about_us", function(req, res){
+    res.sendFile(path.join(__dirname, "about_us.html"));
+});
+
+app.post("/user_login", function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    //res.redirect("/admin_dashboard");
+    res.send("Username:" + username + "\n\nPassword:" + password);
+});
+
+//localhost:8080/product/14578
+app.get("/product/:id", function(req,res){
+    var product_id = req.params.id;
+    res.send("Product ID:" + product_id);
+});
+
+app.get("/admin_dashboard", function(req,res){
+    var obj = {username:"Gabriel", email:"dummyemail@gmail.com"}
+    res.json(obj);
+});
+
 
 //http:localhost:8080
 var port = process.env.PORT || 8080;
